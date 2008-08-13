3256e74c05342850d285907b4b865bfd5d90eccf
TobiasH87 <TobiasH87@5d385b70-8c53-0410-a42a-e57762fca8f8>
2008-08-13 16:08:27 +0000

    Newsreader 1.0.2 für das WBB lite 2 :)

--- wbblite2/portal/de.mailman.wbblite.portal.box.simplePieNewsreader/files/lib/action/NewsreaderCacheAction.class.php
+++ wbblite2/portal/de.mailman.wbblite.portal.box.simplePieNewsreader/files/lib/action/NewsreaderCacheAction.class.php
@@ -13,6 +13,13 @@ class NewsreaderCacheAction extends AbstractAction {
         if ($this->action == 'NewsreaderCache') {
             $urls = preg_split('/\r?\n/', SPNRBOX_FEEDS);
             $cache_location = WBB_DIR.'lib/data/boxes/SimplePieNewsReader/cache';
+
+            // CHARSET
+            if(!defined('CHARSET'))         define('CHARSET', 'UTF-8');
+            if(!defined('SPNRBOX_CHARSET')) define('SPNRBOX_CHARSET', 'UTF-8');
+            if(SPNRBOX_CHARSET == 'default') $charset = CHARSET;
+            else $charset = SPNRBOX_CHARSET;
+
             $feed = new SimplePie();
             $feed->set_feed_url($urls);
             $feed->set_cache_location($cache_location);
@@ -20,10 +27,11 @@ class NewsreaderCacheAction extends AbstractAction {
             $feed->set_cache_duration(0);
             $feed->set_favicon_handler(RELATIVE_WBB_DIR.'lib/data/boxes/SimplePieNewsReader/handler_image.php');
             $feed->set_image_handler(RELATIVE_WBB_DIR.'lib/data/boxes/SimplePieNewsReader/handler_image.php');
+            $feed->set_output_encoding($charset);
             $feed->set_timeout(10);
             $feed->init();
             $feed->handle_content_type();
-            header("Content-type: text/plain; charset=UTF-8");
+            header("Content-type: text/plain; charset=".$charset);
             foreach($urls as $feeds){
                 $feeds = trim($feeds);
                 if(empty($feeds)) continue;
