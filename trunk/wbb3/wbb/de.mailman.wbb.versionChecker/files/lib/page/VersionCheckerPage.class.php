c3f5ad5bbf14d74f4864f4b1b143db7009e9758d
wbb3addons <wbb3addons@5d385b70-8c53-0410-a42a-e57762fca8f8>
2008-08-16 13:35:01 +0000

    de.mailman.wbb.versionChecker: Wieder mal ein Add-On, das fast niemand benötigt :) Mit diesem Add-On können WBB3-Versionsnummern verglichen werden, also welche Version würde z.B. als "neuer" interpretiert werden. Das Add-On ist nicht verlinkt. Zum Aufruf einfach index.php?page=VersionChecker eingeben.

---
+++ trunk/wbb3/wbb/de.mailman.wbb.versionChecker/files/lib/page/VersionCheckerPage.class.php
@@ -0,0 +1,43 @@
+<?php
+require_once(WCF_DIR.'lib/page/AbstractPage.class.php');
+
+class VersionCheckerPage extends AbstractPage {
+	public $templateName = 'versionChecker';
+	public $verFirst = '1.0.0';
+	public $verSecond = '1.0.0 PL 1';
+	public $verResult = -2;
+
+	/**
+	 * @see Page::readParameters()
+	 */
+	public function readParameters() {
+		parent::readParameters();
+		if(!empty($_POST['action']) && $_POST['action'] == 'compare') {
+		    if(!empty($_POST['version1']) && !empty($_POST['version2'])) {
+		        $this->verResult = version_compare($_POST['version1'], $_POST['version2']);
+		        $this->verFirst = $_POST['version1'];
+		        $this->verSecond = $_POST['version2'];
+		    }
+		}
+	}
+
+	/**
+	 * @see Page::assignVariables();
+	 */
+	public function assignVariables() {
+		parent::assignVariables();
+		WCF::getTPL()->assign(array(
+			'verResult' => $this->verResult,
+			'version1' => $this->verFirst,
+			'version2' => $this->verSecond
+		));
+	}
+
+	/**
+	 * @see Page::show()
+	 */
+	public function show() {
+		parent::show();
+	}
+}
+?>
