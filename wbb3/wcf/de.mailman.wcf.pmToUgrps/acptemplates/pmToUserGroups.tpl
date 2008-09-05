a443f5a5f0cb1607b22cac9c306d7ec94eaf5101
wbb3addons <wbb3addons@5d385b70-8c53-0410-a42a-e57762fca8f8>
2008-09-05 08:00:16 +0000

    de.mailman.wcf.pmToUgrps: - ufdada's ACP-Wysiwyg-Editor implementiert

--- wbb3/wcf/de.mailman.wcf.pmToUgrps/acptemplates/pmToUserGroups.tpl
+++ wbb3/wcf/de.mailman.wcf.pmToUgrps/acptemplates/pmToUserGroups.tpl
@@ -1,4 +1,5 @@
 {include file='header'}
+{include file='wysiwyg'}
 <div class="mainHeadline">
 	<img src="{@RELATIVE_WCF_DIR}icon/pmNewL.png" alt="" />
 	<div class="headlineContainer">
@@ -8,6 +9,26 @@
 
 {if $errorField}
 	<p class="error">{lang}wcf.global.form.error{/lang}</p>
+{/if}
+
+{if $preview|isset}
+	<div class="border">
+		<div class="containerHead">
+			<h3>{lang}wcf.message.preview{/lang}</h3>
+		</div>
+		
+		<div class="message content">
+			<div class="messageInner container-1">
+				{if $subject}
+					<h4>{$subject}</h4>
+				{/if}
+				<div class="messageBody">
+					{@$preview}
+				</div>
+			</div>
+		</div>
+	</div>
+	<br />
 {/if}
 
 <form method="post" action="index.php?form=PMToUserGroups">
@@ -34,89 +55,6 @@
 						{/if}
 					</div>
 				</div>
-			</fieldset>
-
-            <fieldset>
-            	<legend>{lang}wcf.pmToUgrps.pm.legend{/lang}</legend>
-            
-            	<div class="formElement{if $errorField == 'subject'} formError{/if}">
-            		<div class="formFieldLabel">
-            			<label for="subject">{lang}wcf.pmToUgrps.pm.subject{/lang}</label>
-            		</div>
-            		<div class="formField">
-            			<input type="text" class="inputText" name="subject" id="subject" value="{$subject}" />
-            			{if $errorField == 'subject'}
-            				<p class="innerError">
-            					{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
-            				</p>
-            			{/if}
-            		</div>
-            	</div>
-
-            	<div class="formElement{if $errorField == 'text'} formError{/if}" id="editor">
-            		<div class="formFieldLabel">
-            			<label for="text">{lang}wcf.pmToUgrps.pm.text{/lang}</label>
-            		</div>
-            		<div class="formField">
-            			<textarea name="text" id="text" rows="20" cols="40">{if $text|isset}{$text}{/if}</textarea>
-            			{if $errorField == 'text'}
-            				<p class="innerError">
-            					{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
-            					{if $errorType == 'tooLong'}{lang}wcf.message.error.tooLong{/lang}{/if}
-            					{if $errorType == 'censoredWordsFound'}{lang}wcf.message.error.censoredWordsFound{/lang}{/if}
-            				</p>
-            			{/if}
-                        {if $extWysiwyg}
-                            <div class="smallButtons">
-                                <ul>
-                                    <li><a href="{@RELATIVE_WBB_DIR}index.php?form=ExternalWysiwygEditor&amp;permissionType=message{@SID_ARG_2ND}" target="_blank"><img src="{@RELATIVE_WBB_DIR}icon/externalWysiwygEditorS.png" alt="" /> <span>{lang}wbb.externalWysiwygEditor.title{/lang}</span></a></li>
-                                </ul>
-                            </div>
-                        {/if}
-            		</div>
-            	</div>
-            </fieldset>
-
-            <fieldset>
-            	<legend>{lang}wcf.pmToUgrps.settings.legend{/lang}</legend>
-                {if $canUseSmilies}
-                    <div class="formCheckBox formElement" id="enableSmiliesDiv">
-                        <div class="formField">
-                        	<label for="enableSmilies">
-                            	<input type="checkbox" name="enableSmilies" id="enableSmilies" value="1"{if !$enableSmilies|empty} checked="checked"{/if}>
-                                {lang}wcf.message.settings.enableSmilies{/lang}
-                            </label>
-                        </div>
-                    </div>
-                {/if}
-                {if $canUseHtml}
-                    <div class="formCheckBox formElement" id="enableHtmlDiv">
-                        <div class="formField">
-                        	<label for="enableHtml">
-                            	<input type="checkbox" name="enableHtml" id="enableHtml" value="1"{if !$enableHtml|empty} checked="checked"{/if}>
-                                {lang}wcf.message.settings.enableHtml{/lang}
-                            </label>
-                        </div>
-                    </div>
-                {/if}
-                {if $canUseBBCodes}
-                    <div class="formCheckBox formElement" id="enableBBCodesDiv">
-                        <div class="formField">
-                        	<label for="enableBBCodes">
-                            	<input type="checkbox" name="enableBBCodes" id="enableBBCodes" value="1"{if !$enableBBCodes|empty} checked="checked"{/if}>
-                                {lang}wcf.message.settings.enableBBCodes{/lang}
-                            </label>
-                        </div>
-                    </div>
-                {/if}
-                <div class="formCheckBox formElement" id="showSignatureDiv">
-                    <div class="formField">
-                    	<label for="showSignature">
-                        	<input type="checkbox" name="showSignature" id="showSignature" value="1"{if !$showSignature|empty} checked="checked"{/if}>
-                            {lang}wcf.message.settings.showSignature{/lang}
-                        </label>
-                    </div>
-                </div>
 
                 <div class="formElement">
                     <div class="formFieldLabel">
@@ -150,6 +88,39 @@
                     //]]>
                 </script>
 
+            	<div class="formElement{if $errorField == 'subject'} formError{/if}">
+            		<div class="formFieldLabel">
+            			<label for="subject">{lang}wcf.pmToUgrps.pm.subject{/lang}</label>
+            		</div>
+            		<div class="formField">
+            			<input type="text" class="inputText" name="subject" id="subject" value="{$subject}" />
+            			{if $errorField == 'subject'}
+            				<p class="innerError">
+            					{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
+            				</p>
+            			{/if}
+            		</div>
+            	</div>
+			</fieldset>
+
+            <fieldset>
+            	<legend>{lang}wcf.pmToUgrps.pm.legend{/lang}</legend>
+            	<div class="formElement{if $errorField == 'text'} formError{/if}" id="editor">
+            		<div class="formFieldLabel">
+            			<label for="text">{lang}wcf.pmToUgrps.pm.text{/lang}</label>
+            		</div>
+            		<div class="formField">
+            			<textarea name="text" id="text" rows="20" cols="40">{if $text|isset}{$text}{/if}</textarea>
+            			{if $errorField == 'text'}
+            				<p class="innerError">
+            					{if $errorType == 'empty'}{lang}wcf.global.error.empty{/lang}{/if}
+            					{if $errorType == 'tooLong'}{lang}wcf.message.error.tooLong{/lang}{/if}
+            					{if $errorType == 'censoredWordsFound'}{lang}wcf.message.error.censoredWordsFound{/lang}{/if}
+            				</p>
+            			{/if}
+            		</div>
+            	</div>
+                {include file='messageFormTabs'}
             </fieldset>
     	</div>
     </div>
@@ -158,6 +129,7 @@
         {@SID_INPUT_TAG}
         <input type="hidden" name="packageID" value="{@PACKAGE_ID}" />
     	<input type="submit" name="send" accesskey="s" value="{lang}wcf.global.button.submit{/lang}" />
+        <input type="submit" name="preview" accesskey="p" value="{lang}wcf.global.button.preview{/lang}" />
     	<input type="reset" name="reset" accesskey="r" value="{lang}wcf.global.button.reset{/lang}" />
     </div>
 </form>
--- wbb3/wcf/de.mailman.wcf.pmToUgrps/files/lib/acp/form/PMToUserGroupsForm.class.php
+++ wbb3/wcf/de.mailman.wcf.pmToUgrps/files/lib/acp/form/PMToUserGroupsForm.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once(WCF_DIR.'lib/acp/form/ACPForm.class.php');
+require_once(WCF_DIR.'lib/acp/form/WysiwygCacheloaderForm.class.php');
 require_once(WCF_DIR.'lib/data/user/group/Group.class.php');
 
 /**
@@ -7,13 +7,12 @@ require_once(WCF_DIR.'lib/data/user/group/Group.class.php');
  * @package de.mailman.wcf.pmToUgrps
  * @author  MailMan (http://wbb3addons.ump2002.net)
  */
-class PMToUserGroupsForm extends ACPForm {
+class PMToUserGroupsForm extends WysiwygCacheloaderForm {
     public $templateName = 'pmToUserGroups';
     public $groups = array();
     public $groupIDs = array();
     public $subject = '';
     public $text = '';
-    public $extWysiwyg = false;
     public $enableSmilies = false;
     public $enableHtml = false;
     public $enableBBCodes = false;
@@ -23,21 +22,19 @@ class PMToUserGroupsForm extends ACPForm {
     public $canUseBBCodes = false;
     public $maxTextLength = 10000;
     public $limit = 25;
-    public $user;
-
+    public $preview, $send, $user;
     /**
      * @see Page::readParameters()
      */
     public function readParameters() {
         parent::readParameters();
-        if(defined('RELATIVE_WBB_DIR') && @is_file(RELATIVE_WBB_DIR.'lib/form/ExternalWysiwygEditorForm.class.php')) $this->extWysiwyg = true;
         $this->canUseSmilies = WCF::getUser()->getPermission('user.message.canUseSmilies');
         $this->canUseHtml = WCF::getUser()->getPermission('user.message.canUseHtml');
         $this->canUseBBCodes = WCF::getUser()->getPermission('user.message.canUseBBCodes');
         if(empty($_POST)) {
-			if($this->canUseSmilies)    $this->enableSmilies = true;
-			if($this->canUseBBCodes)    $this->enableBBCodes = true;
-			$this->showSignature = true;
+            if($this->canUseSmilies)    $this->enableSmilies = true;
+            if($this->canUseBBCodes)    $this->enableBBCodes = true;
+            $this->showSignature = true;
         }
         $this->maxTextLength = WCF::getUser()->getPermission('user.pm.maxLength');
     }
@@ -53,8 +50,36 @@ class PMToUserGroupsForm extends ACPForm {
         if(isset($_POST['enableHtml'])) $this->enableHtml = $_POST['enableHtml'];
         if(isset($_POST['enableBBCodes'])) $this->enableBBCodes = $_POST['enableBBCodes'];
         if(isset($_POST['showSignature'])) $this->showSignature = $_POST['showSignature'];
-		if(isset($_POST['groupIDs']) && is_array($_POST['groupIDs'])) $this->groupIDs = ArrayUtil::toIntegerArray($_POST['groupIDs']);
+        if(isset($_POST['groupIDs']) && is_array($_POST['groupIDs'])) $this->groupIDs = ArrayUtil::toIntegerArray($_POST['groupIDs']);
         if(isset($_POST['limit'])) $this->limit = $_POST['limit'];
+        if(isset($_POST['preview'])) $this->preview = (boolean) $_POST['preview'];
+        if(isset($_POST['send'])) $this->send = (boolean) $_POST['send'];
+    }
+
+    /**
+     * @see Form::submit()
+     */
+    public function submit() {
+        $this->readFormParameters();
+
+        try {
+            // preview
+            if ($this->preview) {
+                require_once(WCF_DIR.'lib/data/message/pm/PMEditor.class.php');
+                WCF::getTPL()->assign('preview', PMEditor::createPreview($this->subject, $this->text, $this->enableSmilies, $this->enableHtml, $this->enableBBCodes));
+                $this->validate();
+            }
+            // send message
+            if ($this->send) {
+                $this->validate();
+                // no errors
+                $this->save();
+            }
+        }
+        catch (UserInputException $e) {
+            $this->errorField = $e->getField();
+            $this->errorType = $e->getType();
+        }
     }
 
 
@@ -62,18 +87,18 @@ class PMToUserGroupsForm extends ACPForm {
      * @see Form::validate()
      */
     public function validate() {
-		if(!count($this->groupIDs)) throw new UserInputException('groupIDs', 'empty');
-		if(empty($this->subject)) throw new UserInputException('subject', 'empty');
-		if(empty($this->text)) {
-		    throw new UserInputException('text', 'empty');
+        if(!count($this->groupIDs)) throw new UserInputException('groupIDs', 'empty');
+        if(empty($this->subject)) throw new UserInputException('subject', 'empty');
+        if(empty($this->text)) {
+            throw new UserInputException('text', 'empty');
         } else if(StringUtil::length($this->text) > $this->maxTextLength) {
-			throw new UserInputException('text', 'tooLong');
-		} else if(defined('ENABLE_CENSORSHIP') && ENABLE_CENSORSHIP) {
+            throw new UserInputException('text', 'tooLong');
+        } else if(defined('ENABLE_CENSORSHIP') && ENABLE_CENSORSHIP) {
             require_once(WCF_DIR.'lib/data/message/censorship/Censorship.class.php');
             $result = Censorship::test($this->text);
             if($result) {
-            	WCF::getTPL()->assign('censoredWords', $result);
-            	throw new UserInputException('text', 'censoredWordsFound');
+                WCF::getTPL()->assign('censoredWords', $result);
+                throw new UserInputException('text', 'censoredWordsFound');
             }
         }
 
@@ -85,31 +110,31 @@ class PMToUserGroupsForm extends ACPForm {
      */
     public function save() {
         parent::save();
-		// save config in session
-		$pmData = WCF::getSession()->getVar('pmData');
-		if($pmData === null) $pmData = array();
-		$pmSessionID = count($pmData);
-		$pmData[$pmSessionID] = array(
-			'groupIDs'  => implode(',', $this->groupIDs),
-			'limit'     => $this->limit,
-			'subject'   => $this->subject,
-			'text'      => $this->text,
+        // save config in session
+        $pmData = WCF::getSession()->getVar('pmData');
+        if($pmData === null) $pmData = array();
+        $pmSessionID = count($pmData);
+        $pmData[$pmSessionID] = array(
+            'groupIDs'  => implode(',', $this->groupIDs),
+            'limit'     => $this->limit,
+            'subject'   => $this->subject,
+            'text'      => $this->text,
             'enableSmilies' => $this->enableSmilies,
             'enableHtml'    => $this->enableHtml,
             'enableBBCodes' => $this->enableBBCodes,
             'showSignature' => $this->showSignature,
             'startTime'     => TIME_NOW
-		);
-		WCF::getSession()->register('pmData', $pmData);
-		$this->saved();
+        );
+        WCF::getSession()->register('pmData', $pmData);
+        $this->saved();
 
-		// show worker template
-		WCF::getTPL()->assign(array(
-			'pageTitle' => WCF::getLanguage()->get('wcf.pmToUgrps.pageTitle'),
-			'url' => 'index.php?action=PMToUserGroups&pmSessionID='.$pmSessionID.'&packageID='.PACKAGE_ID.SID_ARG_2ND_NOT_ENCODED
-		));
-		WCF::getTPL()->display('worker');
-		exit;
+        // show worker template
+        WCF::getTPL()->assign(array(
+            'pageTitle' => WCF::getLanguage()->get('wcf.pmToUgrps.pageTitle'),
+            'url' => 'index.php?action=PMToUserGroups&pmSessionID='.$pmSessionID.'&packageID='.PACKAGE_ID.SID_ARG_2ND_NOT_ENCODED
+        ));
+        WCF::getTPL()->display('worker');
+        exit;
     }
 
     /**
@@ -127,7 +152,6 @@ class PMToUserGroupsForm extends ACPForm {
         parent::assignVariables();
 
         WCF::getTPL()->assign(array(
-            'extWysiwyg'    => $this->extWysiwyg,
             'maxTextLength' => $this->maxTextLength,
             'groupIDs'      => $this->groupIDs,
             'groups'        => $this->groups,
@@ -147,9 +171,9 @@ class PMToUserGroupsForm extends ACPForm {
      * @see Page::show()
      */
     public function show() {
-		// enable menu item
-		WCFACP::getMenu()->setActiveMenuItem('wcf.acp.menu.link.group');
-		Session::resetSessions();
+        // enable menu item
+        WCFACP::getMenu()->setActiveMenuItem('wcf.acp.menu.link.group');
+        Session::resetSessions();
 
         WCF::getUser()->checkPermission('admin.user.canPMToUserGroups');
 
