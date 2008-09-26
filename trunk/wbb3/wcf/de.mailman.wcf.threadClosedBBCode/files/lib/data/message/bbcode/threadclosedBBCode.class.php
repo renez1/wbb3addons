56c75f7a5833bcba034fe4e41136f771b9934cbf
nendilo <nendilo@5d385b70-8c53-0410-a42a-e57762fca8f8>
2008-09-26 20:41:14 +0000

    BBCode threadClosed eingefügt

---
+++ trunk/wbb3/wcf/de.mailman.wcf.threadClosedBBCode/files/lib/data/message/bbcode/threadclosedBBCode.class.php
@@ -0,0 +1,34 @@
+<?php
+/* $Id$ */
+require_once(WCF_DIR.'lib/data/message/bbcode/BBCodeParser.class.php');
+require_once(WCF_DIR.'lib/data/message/bbcode/BBCode.class.php');
+
+/**
+ * Parses the [threadclosed] bbcode Tag.
+ *
+ * @author	Nendilo	
+ * @package	BBCodeThreadClosed
+ */
+class threadclosedBBCode implements BBCode {
+	/**
+	 * @see BBCode::getParsedTag()
+	 */
+	public function getParsedTag($openingTag, $content, $closingTag, BBCodeParser $parser) {
+        if (WCF::getUser()->getPermission('user.message.canUseBBCodeThreadClosed')){
+		    if ($parser->getOutputType() == 'text/html') {
+			    // show template
+			    WCF::getTPL()->assign(array(
+				    'content' => $content,
+			    ));
+			    return WCF::getTPL()->fetch('threadclosedBBCodeTag');
+		    }
+		    else if ($parser->getOutputType() == 'text/plain') {
+			    return $content;
+		    }
+	    }
+	    else {
+	        return false;
+	    }
+	}
+}
+?>
---
+++ trunk/wbb3/wcf/de.mailman.wcf.threadClosedBBCode/templates/threadclosedBBCodeTag.tpl
@@ -0,0 +1,6 @@
+{* $Id$ *}
+<div style="border: 1px solid red;background-color: #fff;text-align: center;">
+        <span style="font-size: 1.2em;font-weight: bold;color: red;">{lang}wcf.bbcode.threadclosed.notice{/lang}</span><br />
+		<img src="{@RELATIVE_WCF_DIR}images/threadClosedNotice.png" alt="{lang}wcf.bbcode.threadclosed.notice{/lang}" /><br />
+        {if $content}<span style="color: red;"><strong>{$this->user->username}:</strong> {@$content}</span>{/if}
+</div>
