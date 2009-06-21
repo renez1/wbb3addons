a9bb533a43fba4af6ead9b652477d24f7f586187
wbb3addons <wbb3addons@5d385b70-8c53-0410-a42a-e57762fca8f8>
2009-06-21 13:16:14 +0000

    Portal2 - StickyTopicsBox - Initial Load

---
+++ trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/event/listener/PortalStickyTopicsBoxListener.class.php
@@ -0,0 +1,29 @@
+<?php
+/* $Id$ */
+require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
+
+/**
+ * Clears the sticky box cache
+ * 
+ * @author	MailMan
+ */
+class PortalStickyTopicsBoxListener implements EventListener {
+	/**
+	 * @see EventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+        if($eventName == 'saved') {
+            $refresh = false;
+            if($className == 'PostEditForm' && isset($eventObj->thread) && $eventObj->thread->isSticky) $refresh = true;
+            else if(isset($eventObj->activeCategory) && $eventObj->activeCategory == 'portal') $refresh = true;
+            if($refresh) {
+                $sql = "SELECT boxID"
+                    ."\n  FROM wbb".WBB_N."_portalboxes"
+                    ."\n WHERE boxName = 'stickyTopics'";
+                list($boxID) = WCF::getDB()->getFirstRow($sql, MYSQL_NUM);
+                if(!empty($boxID)) WCF::getCache()->clear(WBB_DIR.'cache/','cache.box-'.$boxID.'.php');
+            }
+        }
+	}
+}
+?>
