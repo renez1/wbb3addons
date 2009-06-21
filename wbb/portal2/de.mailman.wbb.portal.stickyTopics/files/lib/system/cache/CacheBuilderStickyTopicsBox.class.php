0022e03b1cd85be3d51fa2b627669e96c978c66f
oliver <oliver@kliebisch.net>
2009-06-21 17:52:05 +0000

    -Query etwas verbessert

--- wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/cache/CacheBuilderStickyTopicsBox.class.php
+++ wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/cache/CacheBuilderStickyTopicsBox.class.php
@@ -11,12 +11,13 @@ class CacheBuilderStickyTopicsBox implements CacheBuilder {
 		$data           = array();
         $permBoardIDs   = Board::getAccessibleBoards();
 		if(!STICKYTOPICSBOX_BOARDIDS || empty($permBoardIDs)) return $data;
-
+		$boardIDs = explode(',', $permBoardIDs);
+		$boardIDs = array_intersect($boardIDs, explode(',', STICKYTOPICSBOX_BOARDIDS));
+		
 		$sql = "SELECT *
 		      FROM wbb".WBB_N."_thread
-             WHERE isSticky = 1
-               AND boardID IN(".STICKYTOPICSBOX_BOARDIDS.")
-               AND boardID IN(".$permBoardIDs.")
+             WHERE isSticky = 1               
+               AND boardID IN(".implode(',', $boardIDs).")
                AND isDeleted = 0
                AND isDisabled = 0
                AND movedThreadID = 0";
