a22351f9ebd99783a658b2f13ec41a63a66ccd07
oliver@kliebisch.net <oliver@kliebisch.net@5d385b70-8c53-0410-a42a-e57762fca8f8>
2009-06-22 18:53:29 +0000

    -Cachlöschung überarbeitet (hat aber nichts mit einem Fix zu tun)

--- trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/event/listener/PortalStickyTopicsBoxListener.class.php
+++ trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/event/listener/PortalStickyTopicsBoxListener.class.php
@@ -4,7 +4,7 @@ require_once(WCF_DIR.'lib/system/event/EventListener.class.php');
 
 /**
  * Clears the sticky box cache
- * 
+ *
  * @author	MailMan
  */
 class PortalStickyTopicsBoxListener implements EventListener {
@@ -12,12 +12,18 @@ class PortalStickyTopicsBoxListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-        if($eventName == 'saved') {            
-            if($className == 'PostEditForm' && isset($eventObj->thread) && $eventObj->thread->isSticky) {
-            	$box = PortalBox::getBoxByName('stickyTopics');
-				$box->clearDataCache();                                      
-            }
-        }
+		$thread = null;
+		if ($className == 'ThreadAddForm') {
+			$thread = $eventObj->newThread;
+		}
+		else {
+			$thread = $eventObj->thread;
+		}
+		if ($thread && $eventObj->isImportant == 1) {
+			$box = PortalBox::getBoxByName('stickyTopics');
+			$box->clearDataCache();
+		}
+
 	}
 }
 ?>
