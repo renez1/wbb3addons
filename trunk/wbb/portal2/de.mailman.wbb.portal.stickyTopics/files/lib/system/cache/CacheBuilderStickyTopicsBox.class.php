a25f67360d106c4f1e25c376f50d86d5965a98ff
wbb3addons <wbb3addons@5d385b70-8c53-0410-a42a-e57762fca8f8>
2009-06-23 18:11:38 +0000

    Portal2 - StickyTopicsBox 2.0.0 - Caching angepasst - Anzeige "Keine Themen vorhanden" hinzugefügt wenn die Box leer ist

--- trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/cache/CacheBuilderStickyTopicsBox.class.php
+++ trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/cache/CacheBuilderStickyTopicsBox.class.php
@@ -4,33 +4,27 @@ require_once(WCF_DIR.'lib/data/user/User.class.php');
 require_once(WCF_DIR.'lib/system/cache/CacheBuilder.class.php');
 
 class CacheBuilderStickyTopicsBox implements CacheBuilder {
-	/**
-	 * @see CacheBuilder::getData()
-	 */
-	public function getData($cacheResource) {
-		$data           = array();
-		/**
-		 * FIXME: This is horrible wrong. This way the cached data depends on the user who triggers
-		 * the cache loader.
-		 */
-        $permBoardIDs   = Board::getAccessibleBoards();
-		if(!STICKYTOPICSBOX_BOARDIDS || empty($permBoardIDs)) return $data;
-		$boardIDs = explode(',', $permBoardIDs);
-		$boardIDs = array_intersect($boardIDs, explode(',', STICKYTOPICSBOX_BOARDIDS));
-		
-		$sql = "SELECT *
-		      FROM wbb".WBB_N."_thread
-             WHERE isSticky = 1               
-               AND boardID IN(".implode(',', $boardIDs).")
-               AND isDeleted = 0
-               AND isDisabled = 0
-               AND movedThreadID = 0";
-        if(!STICKYTOPICSBOX_SHOWCLOSED) $sql .= " AND isClosed = 0";
-        $sql .= " ORDER BY ".STICKYTOPICSBOX_SORTFIELD." DESC";              
+    /**
+     * @see CacheBuilder::getData()
+     */
+    public function getData($cacheResource) {
+        $data = array();
 
-		$result = WCF::getDB()->sendQuery($sql, STICKYTOPICSBOX_NUMOFENTRIES);
-		while ($row = WCF::getDB()->fetchArray($result)) $data[] = $row;
+        if(!STICKYTOPICSBOX_BOARDIDS) return $data;
+
+        $sql = "SELECT *
+                  FROM wbb".WBB_N."_thread
+                 WHERE isSticky = 1
+                   AND boardID IN(".STICKYTOPICSBOX_BOARDIDS.")
+                   AND isDeleted = 0
+                   AND isDisabled = 0
+                   AND movedThreadID = 0";
+        if(!STICKYTOPICSBOX_SHOWCLOSED) $sql .= " AND isClosed = 0";
+        $sql .= " ORDER BY ".STICKYTOPICSBOX_SORTFIELD." DESC";
+
+        $result = WCF::getDB()->sendQuery($sql, STICKYTOPICSBOX_NUMOFENTRIES);
+        while ($row = WCF::getDB()->fetchArray($result)) $data[] = $row;
         return $data;
-	}
+    }
 }
 ?>
