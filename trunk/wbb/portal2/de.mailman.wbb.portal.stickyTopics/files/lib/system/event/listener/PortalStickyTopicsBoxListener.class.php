ca8ac348756438c193c9e05ec6e7ee38b461cc72
wbb3addons <wbb3addons@5d385b70-8c53-0410-a42a-e57762fca8f8>
2009-06-25 19:28:32 +0000

    StickyTopicsBox: 2.0.0 PL1 - EventListener angepasst.

--- trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/event/listener/PortalStickyTopicsBoxListener.class.php
+++ trunk/wbb/portal2/de.mailman.wbb.portal.stickyTopics/files/lib/system/event/listener/PortalStickyTopicsBoxListener.class.php
@@ -12,18 +12,26 @@ class PortalStickyTopicsBoxListener implements EventListener {
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		$thread = null;
-		if ($className == 'ThreadAddForm') {
-			$thread = $eventObj->newThread;
+		$clearCache = false;
+		if($className == 'ThreadActionPage' && $eventName == 'show' && isset($eventObj->action) && isset($eventObj->thread)) {
+		    if($eventObj->thread->isSticky == 1
+		    &&($eventObj->action == 'trash'
+		    || $eventObj->action == 'recover'
+		    || $eventObj->action == 'unstick')) {
+		        $clearCache = true;
+            } else if($eventObj->action == 'stick') {
+                $clearCache = true;
+            }
+        } else if($className == 'ThreadAddForm' && $eventName == 'saved' && $eventObj->isImportant == 1) {
+			$clearCache = true;
+        } else if($className == 'PostEditForm' && $eventName == 'saved' && $eventObj->isImportant == 1) {
+			$clearCache = true;
 		}
-		else {
-			$thread = $eventObj->thread;
-		}
-		if ($thread && $eventObj->isImportant == 1) {
+
+		if($clearCache) {
 			$box = PortalBox::getBoxByName('stickyTopics');
 			$box->clearDataCache();
 		}
-
 	}
 }
 ?>
